8dc3831c48baab79dae0727f2b139b06d3d5262c
Ivan Szebenszki <szib77@gmail.com>
2019-02-28 14:11:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget(target, nested) {
+  return document.querySelector(`#nested .target`);
+}
+
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('.ranked-list li')
+  for(var i = 0; i<rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var currentNode = document.querySelector('#grand-node')
+  
+  while (currentNode.children.length !== 0) {
+    currentNode = currentNode.children[0]
+  }
+  
+  return currentNode
+}
