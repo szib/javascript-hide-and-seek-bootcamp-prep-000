3ff9d2e82dc8d626297c34558ff842916b463620
Ivan Szebenszki <szib77@gmail.com>
2019-02-28 13:44:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(target, nested) {
-  var n = document.querySelector(`#nested`);
-  var t = n.querySelectorAll(`.target`)
-  console.log(t[0])
-  return t
-  // .querySelectorAll(`.${target}`)
+  return document.querySelector(`#nested .target`);
 }
